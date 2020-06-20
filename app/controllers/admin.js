e7bda4c6fafee91f481bf4d4d23ed350ed0cc863
filipesrocha1109 <fsrbillie@gmail.com>
2020-06-20 14:44:42 -0300

    1 crud ok

--- app/controllers/admin.js
+++ app/controllers/admin.js
@@ -18,7 +18,11 @@ module.exports.Locais_salvar = function(application, req, res){
     req.assert('EstadoLocal', 'ESTADO Inválido').len(2);
     req.assert('PaisLocal', 'PAIS Inválido').len(2);
 
+    
+
     var erros = req.validationErrors();
+
+    console.log(erros) 
 
     if(erros){
        res.render('admin/form_add_local', {validacao: erros,  local: local});
@@ -54,34 +58,6 @@ module.exports.edtLocal = function(application, req, res){
 module.exports.Locais_editar = function(application, req, res){
     var local = req.body;
 
-    // console.log(local.IDLocal)
-
-    var erros
-
-    if (!local.IDLocal){ erros = erros + 'ID Inválido' }
-    if (!local.NomeLocal){ erros = erros + 'NOME Inválido' }
-    if (!local.NumeroLeitos){ erros = erros + 'NÚMERO DE LEITOS Inválido' }
-    if (!Number.isInteger(!local.NumeroLeitos)){ erros = erros + 'NÚMERO DE LEITOS DEVEM SER INTEIROS' }
-    if (!local.NumeroLeitosDisponiveis){ erros = erros + 'NÚMERO DE LEITOS DISPONÍVEIS Inválido' }
-    if (!Number.isInteger(!local.NumeroLeitosDisponiveis)){ erros = erros + 'NÚMERO DE LEITOS DISPONÍVEIS DEVEM SER INTEIROS' }
-
-
-    //req.assert('TelefoneLocal', 'TELEFONE Inválido').len(14, 15);
-    //req.assert('EmailLocal', 'E-MAIL Inválido').notEmpty();
-    //req.assert('EnderecoLocal', 'ENDEREÇO Inválido').notEmpty();
-    //req.assert('NumeroLocal', 'NÚMERO Inválido').notEmpty();
-    //req.assert('BairroLocal', 'BAIRRO Inválido').notEmpty();
-    //req.assert('CidadeLocal', 'CIDADE Inválido').notEmpty();
-    //req.assert('EstadoLocal', 'ESTADO Inválido').len(2);
-    //req.assert('PaisLocal', 'PAIS Inválido').len(2);
-//
-    //var erros = req.validationErrors();
-//
-    //if(erros){
-    //   res.render('admin/form_edit_local', {validacao: erros,  local: local});
-    //  return;
-    //}
-//
     var connection = application.config.dbconnection();
     var LocaisModel = new application.app.models.LocaisDAO(connection);
 
